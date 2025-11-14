bb4497b0c05f888f07cb3461e43989ea1aeac34a
IamSavitha <savitha.vijayarangan09@gmail.com>
2025-11-14 14:04:11 -0800

    Enhancing UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,10 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './styles/theme.css';
+import './styles/global.css';
+import './styles/animations.css';
+import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
